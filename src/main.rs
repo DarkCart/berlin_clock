7ee24906ffc9836e06030a5154fd52bb4c9354d5
John <minecart94@gmail.com>
2020-02-23 19:41:39 -0600

    added block drawing

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,20 @@ fn main() {
 }
 
 fn display_clock(five_hr: &u32, one_hr: &u32, five_mn: &u32, one_mn: &u32, seconds: &u32) {
-		
+	println!("S {}", seconds);
+	draw_block(&(seconds % 2 == 0));
+	println!("Hx5 {}", five_hr);
+	println!("Hx1 {}", one_hr);
+	println!("Mx5 {}", five_mn);
+	println!("Mx1 {}", one_mn);		
 } 
+
+fn draw_block(on: &bool) {
+	println!("╔══╗");
+	if on == &true {
+		println!("║{0}{0}║", "█");
+	} else {
+		println!("║  ║");
+	}
+	println!("╚══╝");
+}
