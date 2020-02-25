3806de91b5ce73601a7a0ead7035d93489b22495
John <minecart94@gmail.com>
2020-02-24 22:17:25 -0600

    added color

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use chrono::prelude::*;
 use std::{thread, time};
+use colored::*;
 
 fn main() {
-	let mut current_time: DateTime<Local> = Local::now();
+	let mut current_time: DateTime<Local>;
 	loop {
 		current_time = Local::now();
 		print!("\x1B[2J");
@@ -27,9 +28,9 @@ fn display_clock(five_hr: &u32, one_hr: &u32, five_mn: &u32, one_mn: &u32, secon
 fn draw_sec_block(on: &bool) {
 	println!("╔══╗");
 	if on == &true {
-                println!("║{0}{0}║", "█");
+                println!("║{0}{0}║", "█".bright_yellow());
         } else {
-        	println!("║{0}{0}║", "░");
+        	println!("║{0}{0}║", "░".bright_yellow());
         }
 	println!("╚══╝");
 }
@@ -37,7 +38,7 @@ fn draw_sec_block(on: &bool) {
 fn draw_blocks(current_val: &u32, range: &u32) {
 	// looks hacky as hell, this is the only thing
 	// i can think of to get this thing to work
-	for i in 1..=*range {
+	for _i in 1..=*range {
 		print!("╔══╗");
 
 	}
@@ -45,14 +46,14 @@ fn draw_blocks(current_val: &u32, range: &u32) {
 	
 	for j in 1..=*range {
 		if &j <= current_val {
-			print!("║{0}{0}║", "█");
+			print!("║{0}{0}║", "█".bright_red());
 		} else {
-			print!("║{0}{0}║", "░");
+			print!("║{0}{0}║", "░".bright_red());
 		}
 	}
 
 	println!("");
-	for k in 1..=*range {
+	for _k in 1..=*range {
 		print!("╚══╝");
 	}
 	println!("");
